db068c9e4b3ddd431ffb7366822b40dca2c921cc
YueChao <yue327305210@163.com>
2020-04-04 20:12:50 +0800

    updated test case

--- src/test/java/org/example/alvin/service/UserServiceTest.java
+++ src/test/java/org/example/alvin/service/UserServiceTest.java
@@ -1,25 +1,11 @@
 package org.example.alvin.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.util.Assert;
 
-@ContextConfiguration(locations = "../applicationContext.xml")
 public class UserServiceTest {
-
-    @Autowired
     private UserService userService;
-
-    @Test
-    public void hasMatchUserTest() {
-        boolean hasMatchUser1 = userService.hasMatchUser("admin", "123456");
-        boolean hasMatchUser2 = userService.hasMatchUser("admin", "123");
-
-        Assert.isTrue(hasMatchUser1);
-        Assert.isTrue(!hasMatchUser2);
-    }
 
     @Test
     public void componentScanTest() {
