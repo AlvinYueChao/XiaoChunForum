f2fc680d84a8e08a93207783a36105b1b5bad3f2
YueChao <yue327305210@163.com>
2020-05-31 11:45:55 +0800

    updated user domain

--- src/main/java/org/example/alvin/domain/User.java
+++ src/main/java/org/example/alvin/domain/User.java
@@ -1,16 +1,35 @@
 package org.example.alvin.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import java.io.Serializable;
-import java.util.Date;
+import javax.persistence.*;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class User implements Serializable {
-    private Long userId;
+@Entity
+@Table(name = "t_user")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class User extends BaseDomain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "user_name")
     private String userName;
+
+    @Column(name = "password")
     private String password;
-    private Long credits;
-    private String lastIp;
-    private Date lastVisit;
+
+    @Column(name = "user_type")
+    private int userType;
+
+    @Column(name = "locked")
+    private int locked;
+
+    @Column(name = "credits")
+    private int credits;
 }
