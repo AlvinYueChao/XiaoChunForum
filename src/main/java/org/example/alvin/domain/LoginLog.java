6ad33dfc322e1581ca093eaad712c84101d06633
YueChao <yue327305210@163.com>
2020-05-31 11:04:17 +0800

    updated loginlog domain

--- src/main/java/org/example/alvin/domain/LoginLog.java
+++ src/main/java/org/example/alvin/domain/LoginLog.java
@@ -2,14 +2,29 @@ package org.example.alvin.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import java.util.Date;
+import javax.persistence.*;
+import java.time.OffsetDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "t_login_log")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class LoginLog extends BaseDomain {
-    private Long loginLogId;
-    private Long userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "login_log_id")
+    private int loginLogId;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "ip")
     private String ip;
-    private Date loginDate;
+
+    @Column(name = "login_datetime")
+    private OffsetDateTime loginDate;
 }
