06e1eef6ba94fa8e169c9f8933a640c1bfffa380
YueChao <yue327305210@163.com>
2020-06-07 14:02:27 +0800

    removed unless interface

--- src/main/java/org/example/alvin/dao/BoardDao.java
+++ src/main/java/org/example/alvin/dao/BoardDao.java
@@ -1,38 +1,4 @@
 package org.example.alvin.dao;
 
-import org.example.alvin.domain.Board;
-
-import java.util.List;
-
-public class BoardDao implements BaseDao<Board> {
-
-    @Override
-    public void save(Board entity) {
-
-    }
-
-    @Override
-    public void remove(Board entity) {
-
-    }
-
-    @Override
-    public void update(Board entity) {
-
-    }
-
-    @Override
-    public Board load(int id) {
-        return null;
-    }
-
-    @Override
-    public List<Board> load(String query) {
-        return null;
-    }
-
-    @Override
-    public List<Board> load(String query, Object... params) {
-        return null;
-    }
+public class BoardDao {
 }
