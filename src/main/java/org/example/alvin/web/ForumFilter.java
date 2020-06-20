0c0a44b88ac7d10ad8f9045c5aad441399f60457
YueChao <yue327305210@163.com>
2020-06-20 21:53:47 +0800

    filter setup

---
+++ src/main/java/org/example/alvin/web/ForumFilter.java
@@ -0,0 +1,12 @@
+package org.example.alvin.web;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ForumFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
