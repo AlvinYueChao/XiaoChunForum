b4858e6df605831a0adce7a71f73b94e8a8179b4
YueChao <yue327305210@163.com>
2020-06-26 21:29:34 +0800

    updated loginLogDao by repository

--- src/main/java/org/example/alvin/dao/LoginLogDao.java
+++ src/main/java/org/example/alvin/dao/LoginLogDao.java
@@ -3,6 +3,7 @@ package org.example.alvin.dao;
 import lombok.extern.slf4j.Slf4j;
 import org.example.alvin.domain.LoginLog;
 import org.example.alvin.repository.LoginLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Slf4j
@@ -11,6 +12,7 @@ public class LoginLogDao extends BaseDao<LoginLog> {
 
     private LoginLogRepository loginLogRepository;
 
+    @Autowired
     public void setLoginLogRepository(LoginLogRepository loginLogRepository) {
         this.loginLogRepository = loginLogRepository;
     }
